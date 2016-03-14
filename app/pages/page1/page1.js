ce8ded5b90063126e115570ab564d7baf1b2c957
Ryan Marchock <ryan@bynd.com>
2016-03-14 17:40:35 +0000

    quiz

--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -3,6 +3,9 @@ import {forwardRef} from 'angular2/core';
 import {PagePopPage} from '../page-pop/page-pop';
 import {Share} from '../../components/share/share';
 import {Test} from '../../components/test/test';
+import {Quiz} from '../../models/quiz';
+
+
 
 @Page({
   templateUrl: 'build/pages/page1/page1.html',
@@ -19,10 +22,27 @@ export class Page1 {
 
     constructor(nav) {
         this.nav = nav;
+        this.quiz = new Quiz();
+        this.qIndex = 1;
+        this.question = this.quiz.getQuestion(1);
+        this.multipleChoice = this.quiz.getMultipleChoice(1);
+
     }
 
     goToPopPage() {
         this.nav.push(PagePopPage);
+    }
+
+    showAnswer(index) {
+        var num = this.quiz.getAnswer(1);
+
+        console.log(num, index)
+
+        if (num === index) {
+            alert('Correct');
+        } else {
+            alert('Try again');
+        }
     }
 
 
